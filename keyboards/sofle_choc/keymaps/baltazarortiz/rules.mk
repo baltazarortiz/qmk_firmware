34475a17749b10c2d558de22173f02ce26db54a9
Baltazar Ortiz <baltazar@mit.edu>
2023-08-02 13:56:44 -0700

    Add personal map

---
+++ keyboards/sofle_choc/keymaps/baltazarortiz/rules.mk
@@ -0,0 +1,20 @@
+CONSOLE_ENABLE = yes
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+
+TRI_LAYER_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+OLED_ENABLE = yes
+
+# Enable per-key lighting
+RGB_MATRIX_ENABLE = yes
+
+# Enable audio keys and system control
+EXTRAKEY_ENABLE = yes
+
+# Enable link time optimization
+# Trade longer compile time for smaller firmware size
+LTO_ENABLE = yes
